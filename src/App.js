8cb81f9476be3bc527b61c337ea1483b9742c9ff
이다은 <eun000221@gmail.com>
2024-10-11 14:08:37 +0900

    Ch08-2 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,66 @@
+// import React from "react";
+// import "./App.css";
+// import { HashRouter, Route } from 'react-router-dom'
+// import About from './routes/About'
+
+// function App() {
+//   return (
+//     <HashRouter>
+//       <Route path="/about" Component={About}/>
+//     </HashRouter>
+//   )
+// }
+// export default App
+
+
+
 import React from "react";
-import Home from "./routes/Home";
 import "./App.css";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home'
 
 function App() {
-  return <Home />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
 }
-export default App
+
+export default App;
+
+
+// import React from "react";
+// import "./App.css";
+// import { HashRouter, Routes, Route } from 'react-router-dom';
+// import About from './routes/About';
+// import Home from './routes/Home'
+
+// function App() {
+//   return (
+//     <HashRouter>
+//       {/* <Route path="/home">
+//         <h1>Route</h1>
+//       </Route>
+//       <Route path="/home/introduction">
+//         <h1>introduction</h1>
+//       </Route>
+//       <Route path="/home/about">
+//         <h1>about</h1>
+//       </Route> */}
+//       <Routes> {/* Routes로 Route를 감쌉니다 */}
+//         <Route path="/home" element={<h1>home</h1>} />
+//         <Route path="/home/introduction" element={<h1>introduction</h1>} />
+//         <Route path="/home/about" element={<h1>about</h1>} />
+//       </Routes>
+//     </HashRouter>
+
+//   );
+// }
+
+// export default App;
+
